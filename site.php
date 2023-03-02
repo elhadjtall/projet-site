8b6bf82f38a2cfefc6576331abf46622aa1c9017
Your Name <you@example.com>
2023-03-03 00:38:35 +0100

    projet site + modif

---
+++ site.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doubhel Consulting</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li id="logo"> <a href="#"><img src="logo.jpg" alt="logo" width=50px></a></li>
+                <li><a href="#">LE CABINET</a></li>
+                <li><a href="#">NOS METIERS</a></li>
+                <li><a href="#">CONSEILS</a></li>
+                <li><a href="#">REALISATIONS</a></li>
+                <li><a href="#">CONTACTEZ-NOUS</a></li>
+            </ul>
+        </nav>
+       <div id="imagePrincipal">
+        <h1>DOUBHEL CONSULTING</h1>
+        <div id="premierTrait"></div>
+        <h3>Ingéniérie Génie Civil et Architecture</h3>
+       </div>
+    </header>
+    <section id="presentation">
+        <div id="textIntro"></div>
+        <h2>ASSISTANCE</h2>
+        <h2>EXPERTISE</h2>
+        <h2>ACCOMPAGNEMENT</h2>
+        <P>Un bureau d'étude qui vous assure la structure de vos habitats</P>
+        <p>Un support pour la gestion de vos projets de tous types</p>
+        <button>NOS REALISATIONS</button>
+        <button>EN SAVOIR PLUS</button>
+    </section>
+    <div>
+        <h1>NOS DOMAINES D'ACTIVITE</h1>
+        <div class="tertiaire">
+        <a href="#" class="block1"></a>
+        <p>SECTEUR</p>
+        <p>tertiaire</p>
+        </div>
+        
+    </div>
+</body>
+</html>
