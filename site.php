49e13d6ef97d4e261646c86bacea943f1f1c9f02
Your Name <you@example.com>
2023-03-03 18:47:33 +0100

    Mon projet site + modif

--- site.php
+++ site.php
@@ -4,44 +4,63 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Doubhel Consulting</title>
+    <title>Site Vitrine</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li id="logo"> <a href="#"><img src="logo.jpg" alt="logo" width=50px></a></li>
-                <li><a href="#">LE CABINET</a></li>
-                <li><a href="#">NOS METIERS</a></li>
-                <li><a href="#">CONSEILS</a></li>
-                <li><a href="#">REALISATIONS</a></li>
-                <li><a href="#">CONTACTEZ-NOUS</a></li>
-            </ul>
-        </nav>
-       <div id="imagePrincipal">
-        <h1>DOUBHEL CONSULTING</h1>
-        <div id="premierTrait"></div>
-        <h3>Ingéniérie Génie Civil et Architecture</h3>
-       </div>
-    </header>
-    <section id="presentation">
-        <div id="textIntro"></div>
-        <h2>ASSISTANCE</h2>
-        <h2>EXPERTISE</h2>
-        <h2>ACCOMPAGNEMENT</h2>
-        <P>Un bureau d'étude qui vous assure la structure de vos habitats</P>
-        <p>Un support pour la gestion de vos projets de tous types</p>
-        <button>NOS REALISATIONS</button>
-        <button>EN SAVOIR PLUS</button>
-    </section>
-    <div>
-        <h1>NOS DOMAINES D'ACTIVITE</h1>
-        <div class="tertiaire">
-        <a href="#" class="block1"></a>
-        <p>SECTEUR</p>
-        <p>tertiaire</p>
+    <nav>
+        <h1>Burgure</h1>
+        <div class="onglets">
+            <a href="#">Home</a>
+            <a href="#products">Nos produits</a>
+            <a href="#contacts">Contacts</a>
         </div>
-        
-    </div>
+    </nav>
+    
+    <header>
+        <h1>Burgure</h1>
+        <h4>LE MEILLEUR RESTAURANT BURGER DE PARIS.</h4>
+        <button>Parcourir</button>
+    </header>
+    <section class="main" id="products">
+        <div class="content">
+            <div class="card">
+                <div class="left">
+                    <h1>Nos Valeurs</h1>
+                    <p>Mangez sainement mais délicieusement. Nos clients sont rois, nos clients sont nos patrons.</p>
+                </div>
+                <div class="right">
+                    <img src="image1.jpg" alt="">
+                </div>
+            </div>
+            <div class="card">
+                <div class="left">
+                    <h1>Nos Recettes</h1>
+                    <p>Nous mettons à votre disposition les recettess de nos burges les .</p>
+                </div>
+                <div class="right">
+                    <img src="image2.jpg" alt="">
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <footer>
+        <h1>Nos Services:</h1>
+        <div class="services">
+            <div class="service">
+                <h3>Livraison Gratuite</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat nulla itaque ipsa reprehenderit perspiciatis officia quibusdam tenetur similique quaerat incidunt hic, nemo et cumque. Dicta dolorum alias accusamus deleniti ex?</p>
+            </div>
+            <div class="service">
+                <h3>Payement en Ligne</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita veritatis porro voluptas sequi odit ducimus. Ipsam enim doloribus vero, pariatur non labore harum repellendus repellat, velit qui exercitationem perspiciatis nemo!</p>
+            </div>
+            <div class="service">
+                <h3>Aimé ou remboursé</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quasi itaque doloribus porro explicabo esse suscipit possimus incidunt non! Dicta quidem unde beatae explicabo deleniti quae eos totam delectus repellendus!</p>
+        </div>
+        <p id="contacts">Contact: 06 05 75 15 96 | &copy; 2023, Burgure.</p>
+    </footer>
 </body>
 </html>
